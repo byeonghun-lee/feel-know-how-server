91b46430ae8da5430917f2cde17b4ec36b4c5e10
byeonghun <byeonghun08@gmail.com>
2020-11-01 22:15:07 +0900

    auth api 세팅

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 import serverless from "serverless-http";
 import Koa from "koa";
-import db from "db";
+import bodyParser from "koa-bodyparser"
 import api from "api";
 
 const app = new Koa();
 
-app.use(async ctx => {
-    console.log("db", db);
-    await db.connect();
-    ctx.body = "Hello world!";
-});
-app.use(api.routes);
+app.use(bodyParser());
+app.use(api.routes());
 
 if(process.env.APP_ENV === "local") {
     app.listen(4000, (err) => {
