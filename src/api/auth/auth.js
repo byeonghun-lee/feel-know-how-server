a8b6edae3abb76d877a82faacba1659e199d1f4f
byeonghun <byeonghun08@gmail.com>
2021-03-10 23:05:28 +0900

    auth model schema 오타 수정

--- src/api/auth/auth.js
+++ src/api/auth/auth.js
@@ -1,39 +1,41 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import config from "config";
 import Joi from "joi";
 
 const saltRounds = 10;
 
-const AuthScema = new Schema({
+const AuthSchema = new Schema({
     email: { type: String },
     phoneNumber: { type: String },
+    nickname: {type: String},
     hasdedPassword: { type: String },
 });
 
-AuthScema.methods.setPassword = async function (password) {
+AuthSchema.methods.setPassword = async function (password) {
     const hash = await bcrypt.hash(password, saltRounds);
 
     this.hasdedPassword = hash;
 };
 
-AuthScema.methods.checkPassword = async function (password) {
+AuthSchema.methods.checkPassword = async function (password) {
     const result = await bcrypt.compare(password, this.hasdedPassword);
 
     return result;
 };
 
-AuthScema.methods.serialize = function () {
+AuthSchema.methods.serialize = function () {
     const data = this.toJSON();
     delete data.hasdedPassword;
     return data;
 };
 
-AuthScema.methods.generateToken = function () {
+AuthSchema.methods.generateToken = function () {
     const token = jwt.sign(
         {
             _id: this.email || this.phoneNumber,
+            nickname: this.nickname
         },
         config.JWT_SECRET,
         { expiresIn: "7d" }
@@ -42,7 +44,7 @@ AuthScema.methods.generateToken = function () {
     return token;
 };
 
-AuthScema.statics.findByIdentity = async function (id) {
+AuthSchema.statics.findByIdentity = async function (id) {
     const emailSchema = Joi.string().email();
     const checkEmail = emailSchema.validate(id);
 
@@ -53,5 +55,5 @@ AuthScema.statics.findByIdentity = async function (id) {
     return await this.findOne(query);
 };
 
-const Auth = mongoose.model("Auth", AuthScema);
+const Auth = mongoose.model("Auth", AuthSchema);
 export default Auth;
