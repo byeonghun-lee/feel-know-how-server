ee961dd75c54d2cdb18489b401b562ca25e9b162
byeonghun <byeonghun08@gmail.com>
2021-04-14 22:07:11 +0900

    mailAuth model 생성

---
+++ src/api/mailAuth/mailAuth.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+
+const mailAuthSchema = new Schema({
+    email: { type: String, required: true },
+    code: { type: String, required: true },
+    expireAt: { type: Date },
+});
+
+mailAuthSchema.index({ expireAt: 1, expireAfterSeconds: 0 });
+
+const MailAuth = mongoose.model("MailAuth", mailAuthSchema);
+export default MailAuth;
