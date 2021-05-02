5c0a4486f5225483e11c0b6b36b9eb8101ea483d
byeonghun <byeonghun08@gmail.com>
2021-05-02 22:51:07 +0900

    서버 구동시 날짜와 aws 초기화 추가

--- src/app.js
+++ src/app.js
@@ -4,10 +4,30 @@ import bodyParser from "koa-bodyparser";
 import api from "api";
 import cors from "@koa/cors";
 import jwtMiddleware from "lib/jwtMiddleware";
+import { initAws } from "aws";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
 
 const app = new Koa();
 
-app.use(cors());
+// const corsOptions = {
+//     origin:
+//         process.env.APP_ENV === "local"
+//             ? "http://localhost:3001"
+//             : "https://www.ohmydrawer.com",
+//     credentials: true,
+// };
+
+initAws();
+dayjs.locale("ko");
+global.Day = dayjs;
+
+app.use(
+    cors({
+        origin: (ctx) => ctx.request.header.origin,
+        credentials: (ctx) => true,
+    })
+);
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(api.routes());
