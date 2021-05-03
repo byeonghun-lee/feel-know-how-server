2b36f072075391f25b87f883a9ff6d1e6f36e9cc
byeonghun <byeonghun08@gmail.com>
2021-05-03 23:11:27 +0900

    public drawer static method 추가

--- src/api/drawer/drawer.js
+++ src/api/drawer/drawer.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const DrawerSchema = new Schema({
     name: { type: String, required: true, maxlength: 15 },
-    desc: { type: String, maxLength: 140 },
+    desc: { type: String, maxLength: 141 },
     userId: { type: mongoose.Types.ObjectId, required: true },
     allPublic: { type: Boolean, default: false },
     originDrawerId: { type: mongoose.Types.ObjectId },
@@ -12,9 +12,9 @@ const DrawerSchema = new Schema({
     dateToBeDeleted: { type: Date },
     createdAt: { type: Date, default: new Date() },
     contributors: [{ type: mongoose.Types.ObjectId }],
-    tags: [{ type: String }],
-    forkCount: { type: Number },
-    likeCount: { type: Number },
+    tags: [{ type: String, maxlength: 6 }],
+    forkList: [{ type: mongoose.Types.ObjectId }],
+    likeList: [{ type: mongoose.Types.ObjectId }],
     history: [
         {
             userId: { type: String },
@@ -25,6 +25,13 @@ const DrawerSchema = new Schema({
             },
         },
     ],
+    views: [
+        {
+            yearMonth: { type: String }, // 202101
+            firstHalfMonth: { type: Number, default: 0 },
+            secondHalfMonth: { type: Number, default: 0 },
+        },
+    ],
 });
 
 DrawerSchema.pre("save", function (next) {
@@ -33,6 +40,32 @@ DrawerSchema.pre("save", function (next) {
 
     next();
 });
+
+DrawerSchema.statics.findPublicDrawers = async function ({ skip }) {
+    const query = {
+        allPublic: true,
+    };
+
+    // todo
+    // 날짜별로 sort하고 싶은데...
+    // yearMonth를 키 값으로 해야하나?
+
+    const result = { totalCount: 0, list: [] };
+    result.totalCount = await this.countDocuments(query);
+    const list = await this.find()
+        .select(["name", "desc", "forkList", "likeList"])
+        .limit(30)
+        .skip(skip)
+        .lean();
+    result.list = list.map((drawer) => ({
+        name: drawer.name,
+        desc: drawer.desc,
+        forkCounts: drawer.forkList ? drawer.forkList.length : 0,
+        likeCounts: drawer.likeList ? drawer.likeList.length : 0,
+    }));
+
+    return result;
+};
 
 const Drawer = mongoose.model("Drawer", DrawerSchema);
 export default Drawer;
