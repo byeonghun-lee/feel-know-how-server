b919606f6730269c9b8722bf5ef76ad8f1df5a5e
byeonghun <byeonghun08@gmail.com>
2024-03-22 00:02:46 +0900

    SnsProfile 검색에 checkLogin 추가

--- src/api/search/index.js
+++ src/api/search/index.js
@@ -1,11 +1,16 @@
 import Router from "koa-router";
 import * as searchCtrl from "api/search/search.ctrl";
+import checkedLogin from "lib/checkedLogin";
 
 const search = new Router();
 
 search.get("/", searchCtrl.search);
 search.get("/youtube-channel", searchCtrl.searchYouTubeChannel);
-search.get("/sns-profiles", searchCtrl.searchSnsProfileAndGroup);
-search.get("/sns-profiles/histories", searchCtrl.getHistoryForSnsProfile);
+search.get("/sns-profiles", checkedLogin, searchCtrl.searchSnsProfileAndGroup);
+search.get(
+    "/sns-profiles/histories",
+    checkedLogin,
+    searchCtrl.getHistoryForSnsProfile
+);
 
 export default search;
